df205aa730a736a2bcd664e25b9b85d308c1055a
HarryJPG <70177823+HarryJPG@users.noreply.github.com>
2021-09-15 15:36:25 +1200

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -10,7 +10,13 @@ public class GUI
 {
     private Orders od;
     private Food fd;
-
+    private boolean foodMenuOpen = false;
+    private boolean sidesMenuOpen = false;
+    private boolean drinksMenuOpen = false;
+    static final double BOX_X = 50;
+    static final double BOX_Y = 20;
+    static final double BOX_WIDTH = 120;
+    static final double BOX_HEIGHT = 40;
     /**
      * Constructor for objects of class GUI
      */
@@ -19,26 +25,87 @@ public class GUI
         // initialise instance variables
         od = new Orders();
         UI.initialise();
+        UI.setMouseListener(this::doMouse);
         menu();
     }
     
     /**
-     * Create dropdown menus
+     * Create main menu
      */
     public void menu()
     {
-        double boxX = 0;
-        double boxY = 0;
-        double boxHeight = 30;
-        double boxWidth = 50;
-        UI.drawRect(boxX, boxY, boxHeight, boxWidth);
+        UI.drawRect(BOX_X, BOX_Y, BOX_WIDTH, BOX_HEIGHT);
+        UI.drawRect(BOX_X + BOX_WIDTH + 20, BOX_Y, BOX_WIDTH, BOX_HEIGHT);
+        UI.drawRect(BOX_X + 2*BOX_WIDTH + 40, BOX_Y, BOX_WIDTH, BOX_HEIGHT);
     }
     
     /**
-     * Add food to orders
+     * Dropdown food menu
      */
-    public void addFood()
+    public void ddmFood()
     {
-        
+        for (int i = 1; i <= 4; i++)
+        {
+            UI.setColor(Color.black);
+            UI.drawRect(BOX_X, BOX_Y+BOX_HEIGHT*i, BOX_WIDTH, BOX_HEIGHT);
+            foodMenuOpen = true;
+        }
+    }
+    
+    /**
+     * Dropdown sides menu
+     */
+    public void ddmSides()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            UI.setColor(Color.black);
+            UI.drawRect(BOX_X + BOX_WIDTH + 20, BOX_Y+BOX_HEIGHT*i, BOX_WIDTH, BOX_HEIGHT);
+        }
+    }
+    
+    /**
+     * Dropdown drinks menu
+     */
+    public void ddmDrinks()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            UI.setColor(Color.black);
+            UI.drawRect(BOX_X + 2*BOX_WIDTH + 40, BOX_Y+BOX_HEIGHT*i, BOX_WIDTH, BOX_HEIGHT);
+        }
+    }
+    
+    /**
+     * Hide dropdown menu
+     */
+    public void hide()
+    {
+        UI.setColor(Color.white);
+        UI.fillRect(BOX_X, BOX_Y + BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT*4);
+        UI.setColor(Color.black);
+    }
+    
+    /**
+     * Check to see if any dropdown menu is being clicked
+     */
+    public void doMouse(String action, double x, double y)
+    {
+        if (action.equals("clicked"))
+            {
+                if ((x >= BOX_X) &&
+                    (x <= BOX_X + BOX_WIDTH) &&
+                    (y >= BOX_Y) &&
+                    (y <= BOX_Y + BOX_HEIGHT))
+                {
+                    if (foodMenuOpen == false)
+                        ddmFood();
+                    else
+                    {    
+                        foodMenuOpen = false;
+                        hide();
+                    }
+                }
+            }
     }
 }
